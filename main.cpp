44378dfcdd0bdc7ff4227af6fc561fda4d23fafd
Khalil Santana <khalilsantana@hotmail.com>
2021-08-16 22:43:29 -0300

    feat: naive implementation of weighted grayscale

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include "opencv4/opencv2/opencv.hpp"
 using namespace cv;
 
+const double GRAYSCALE_RED_FACTOR   = 0.299;
+const double GRAYSCALE_GREEN_FACTOR = 0.587;
+const double GRAYSCALE_BLUE_FACTOR  = 0.114;
+
 Mat arithmetic_grayscale(Mat img) {
 	cv::Size size = img.size();
 	int width = size.width;
@@ -13,6 +17,26 @@ Mat arithmetic_grayscale(Mat img) {
 		   Vec3b rgb = img.at<Vec3b>(w, h);
 		   // Arithmetic gray
 		   int gray = (rgb[0] + rgb[1] + rgb[2])/3;
+		   // Write value to outputImg
+		   outputImg.at<uchar>(Point(w, h)) = gray;
+		}
+	}
+	return outputImg;
+}
+
+Mat weighted_grayscale(Mat img) {
+	cv::Size size = img.size();
+	int width = size.width;
+	int height = size.height;
+	Mat outputImg(width, height, CV_8UC1, Scalar(0));
+	for (int w=0; w<width; w++) {
+		for (int h=0; h<height; h++) {
+		   // Read inputImg pixel at (w, h)
+		   Vec3b rgb = img.at<Vec3b>(w, h);
+		   // Weighted airthmetic gray
+		   int gray =  (float)rgb[0] * GRAYSCALE_RED_FACTOR;
+		   gray += (float)rgb[1] * GRAYSCALE_GREEN_FACTOR;
+		   gray += (float)rgb[2] * GRAYSCALE_BLUE_FACTOR;
 		   // Write value to outputImg
 		   outputImg.at<uchar>(Point(w, h)) = gray;
 		}
@@ -35,7 +59,7 @@ int main(int argc, char** argv ) {
     }
     const int channels = image.channels();
     printf("Number of channels = %d\n", channels);
-    Mat arith_gray = arithmetic_grayscale(image);
+    Mat arith_gray = weighted_grayscale(image);
     namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", arith_gray);
     waitKey(0);
