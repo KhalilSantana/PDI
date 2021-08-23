f60fe97a129836c8f704dc4c0063cdc1039d2740
Khalil Santana <khalilsantana@hotmail.com>
2021-08-23 20:48:05 -0300

    Add change main to test every function

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,36 @@ int main(int argc, char **argv) {
     // }
     Mat image, image2;
     image = imread(argv[1], 1);
-    // image2 = imread(argv[2], 1);
+    image2 = imread(argv[2], 1);
     if (!image.data) {
         printf("No image data \n");
         return -1;
     }
-    const int channels = image.channels();
-    printf("Number of channels = %d\n", channels);
-    Mat output = threshold(weighted_grayscale(image),150);
-    namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display Image", output);
-    waitKey(0);
+    // Arithmetic operations
+    Mat addImg = add(image, image2);
+    Mat subImg = sub(image, image2);
+    Mat mulImg = mul(image, image2);
+    Mat divImg = div(image, image2);
+    imwrite("output/add.png", addImg);
+    imwrite("output/sub.png", subImg);
+    imwrite("output/mul.png", mulImg);
+    imwrite("output/div.png", divImg);
+    // // Basic operations
+    Mat arithGrayImg = arithmetic_grayscale(image);
+    Mat weightedGrayImg = weighted_grayscale(image);
+    Mat threshImg = threshold(weightedGrayImg, 150);
+    Mat redImg = isolate_channel(image, 2);
+    Mat greenImg = isolate_channel(image, 1);
+    Mat blueImg = isolate_channel(image, 0);
+    imwrite("output/arith-grayscale.png", arithGrayImg);
+    imwrite("output/weigh-grayscale.png", weightedGrayImg);
+    imwrite("output/threshold.png", threshImg);
+    imwrite("output/red-channel.png", redImg);
+    imwrite("output/green-channel.png", greenImg);
+    imwrite("output/blue-channel.png", blueImg);
+    // namedWindow("Display Image", WINDOW_AUTOSIZE);
+    // imshow("Display Image", output);
+    // waitKey(0);
+    // destroyWindow("Display Image");
     return 0;
 }
